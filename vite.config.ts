4adab5aec268dad3861e67c7713aa4b0cd6f24bc
Cr0wly <wisest.ivan@gmail.com>
2024-06-02 00:30:32 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "toggle_clicker_twa",
+  base: "/toggle_clicker_twa/",
 });
